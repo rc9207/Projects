d83c0418b64929a640076efb588f12053318db20
rc9207 <43714188+rc9207@users.noreply.github.com>
2018-12-10 02:13:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,47 @@ grd.addColorStop(1, "white");
 // Fill with gradient
 ctx.fillStyle = grd;
 ctx.fillRect(50, 50, 150, 80);
+
+
+// Ceasar Cipher
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var fullAlphabet = alphabet + alphabet + alphabet;
+
+function runCipher(){
+  var cipherInput = $('#cypher').val();
+  var cipherOffset = $('#offset').val();
+  cipherOffset = (cipherOffset % alphabet.length);
+  var cipherResult = '';
+
+  for(i=0; i<cipherInput.length; i++){
+     var letter = cipherInput[i];
+     var upper = (letter == letter.toUpperCase());
+     letter = letter.toLowerCase();
+
+     var index = alphabet.indexOf(letter);
+     if(index == -1){
+       cipherResult += letter;
+     } else {
+       index = ((index + cipherOffset) + alphabet.length);
+       var nextLetter = fullAlphabet[index];
+       if(upper) nextLetter = nextLetter.toUpperCase();
+       cipherResult += nextLetter;
+     }
+  }
+
+  $('#Result').val(cipherResult);
+}
+
+$(document).ready(function() {
+  $('#cypher').keypress(function(){
+    setTimeout(function(){ runCipher(); },20);
+  });
+  $('#cypher').blur(function(){
+    runCipher();
+  });
+  $('#offset').change(function(){
+    setTimeout(runCipher(),20);
+  });
+
+});
